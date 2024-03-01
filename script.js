6e17a8ae66fcc88e9887a5da95443338a140c77c
Reika Akuzawa <l06981@googlemail.com>
2024-03-01 22:48:33 +0100

    Add Transition screen animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Start Screen
 const startScreen = document.querySelector('.screen--start');
 const opponentBtns = startScreen.querySelectorAll('.opponent-btn');
 const symbolBtns = startScreen.querySelectorAll('.symbol-btn');
@@ -5,6 +6,9 @@ const backToOpponentBtn = startScreen.querySelector('.back--opponent');
 const selectOpponentSection = startScreen.querySelector('.select--opponent');
 const selectSymbolSection = startScreen.querySelector('.select--symbol');
 const startGameText = startScreen.querySelector('.start-game');
+
+// Game Screen
+const gameScreen = document.querySelector('.screen--game');
 
 opponentBtns.forEach(btn => {
   btn.addEventListener('click', (e) => selectAndShowNextQuestion(e, 'opponent', btn));
@@ -23,26 +27,40 @@ function selectAndShowNextQuestion(e, item, btn) {
     
     if(item === 'opponent') {
         const opponent = btn.dataset.opponent;
-        selectOpponentSection.classList.add('hidden');
-        selectSymbolSection.classList.remove('hidden');
+        hide(selectOpponentSection);
+        show(selectSymbolSection);
     } else if(item === 'symbol') {
         const symbol = btn.dataset.symbol;
-        selectSymbolSection.classList.add('hidden');
-        startGameText.classList.remove('hidden');
+        hide(selectSymbolSection);
+        show(startGameText);
 
         transitionScreen();    
     } 
 }
 
 function transitionScreen() {
-    setTimeout(() => moveLeft(startGameText), 800);
-    setTimeout(() => coverScreen(startScreen), 1600);
+    setTimeout(() => moveLeft(startGameText), 500);
+    setTimeout(() => coverScreen(startScreen), 1000);
+    setTimeout(() => moveDown(startScreen), 1800);
+    setTimeout(() => show(gameScreen), 1400);
 }
 
 function moveLeft(item) {
     item.classList.add('move-left');
 }
 
+function moveDown(item) {
+    item.classList.add('move-down');
+}
+
 function coverScreen(item) {
     item.classList.add('expand');
+}
+
+function show(item) {
+    item.classList.remove('hidden');
+}
+
+function hide(item) {
+    item.classList.add('hidden');
 }
