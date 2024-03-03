1aad42c2e90465eecb3e6be1232a5f70886a16f8
Reika Akuzawa <l06981@googlemail.com>
2024-03-03 12:58:17 +0100

    Add isPlayerSelectedEmptyCell-function and add error msg in game screen

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,19 +1,27 @@
 import {opponent, selectedSymbol, opponentSymbol, gameScreen, userSymbolSign, opponentSymbolSign, settings, checkPlayer} from './settings.js';
-import {hide, show, moveLeft, coverScreen, moveDown, addSelectedIcon} from './helpers.js';
+import {hide, show, moveLeft, coverScreen, moveDown, addSelectedIconToElement} from './helpers.js';
 
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
-
+let gameBoard = [null, null, null, null, null, null, null, null, null];
+const gameStatusError = gameScreen.querySelector('.game-status__text--error');
 const cells = gameScreen.querySelectorAll('.board__cell');
 
 cells.forEach((cell) => {
-    const cellIcon = cell.querySelector('i');
+    const cellIconElement = cell.querySelector('i');
     cell.addEventListener('click', () => {
+        let isPlayerTurnFinished = false;
+
+        // User's turn
         if(settings.turnCount % 2 === 0) {
-            addSelectedIcon(cellIcon, selectedSymbol);
-            addMoveToBoard(cell.dataset.index, selectedSymbol);
+            isPlayerTurnFinished = addMoveToBoard(cell.dataset.index, selectedSymbol, cellIconElement);
+            
+        // Opponent's turn
         } else {
-            addSelectedIcon(cellIcon, opponentSymbol);
-            addMoveToBoard(cell.dataset.index, opponentSymbol);
+            isPlayerTurnFinished = addMoveToBoard(cell.dataset.index, opponentSymbol, cellIconElement);
+        }
+        
+        // If player selected non-empty cell
+        if(!isPlayerTurnFinished) {
+            return;
         }
         settings.turnCount += 1;
 
@@ -25,7 +33,23 @@ cells.forEach((cell) => {
     });
 });
 
-function addMoveToBoard(cellIndex, symbol) {
+function addMoveToBoard(cellIndex, symbol, cellIconElement) {
+    if(isPlayerSelectedEmptyCell(cellIndex)) {
+        addPlayerMoveToBoardArray(cellIndex, symbol);
+        addSelectedIconToElement(cellIconElement, symbol);
+        hide(gameStatusError);
+        return true;
+    } else {
+        show(gameStatusError);
+        return false;
+    }
+}
+
+function isPlayerSelectedEmptyCell(cellIndex) {
+    return gameBoard[cellIndex] === null;
+}
+
+function addPlayerMoveToBoardArray(cellIndex, symbol) {
     gameBoard[cellIndex] = symbol;
 }
 
