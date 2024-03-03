85742e007d89b0968ecc483dcd6fb84a21f7e35f
Reika Akuzawa <l06981@googlemail.com>
2024-03-03 10:20:45 +0100

    Add helper functions for animation and styling

---
+++ src/modules/helpers.js
@@ -0,0 +1,41 @@
+const title = document.querySelector('.title');
+
+function moveLeft(item) {
+    item.classList.add('move-left');
+}
+
+function moveDown(item) {
+    item.classList.add('move-down');
+    hide(title);
+}
+
+function coverScreen(item) {
+    item.classList.add('expand');
+}
+
+function show(item) {
+    item.classList.remove('hidden');
+}
+
+function hide(item) {
+    item.classList.add('hidden');
+}
+
+function addSelectedIcon(element, icon) {
+    if(icon === 'x') {
+        element.classList.add('fa-xmark');
+    } else if(icon === 'o') {
+        element.classList.add('fa-o');
+    } else if(icon === 'manual') {
+        element.classList.add('fa-person');
+    } else if(icon === 'auto') {
+        element.classList.add('fa-computer');
+    }
+}
+
+function getOpponentSymbol(selectedSymbol) {
+    return selectedSymbol === 'x' ? 'o' : 'x';
+}
+
+export {moveLeft, moveDown, coverScreen, show, hide, addSelectedIcon, getOpponentSymbol};
+
