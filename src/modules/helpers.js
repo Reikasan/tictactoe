05b98cbfb7f5b3d149b17e06a774a32e825d6fb8
Reika Akuzawa <l06981@googlemail.com>
2024-03-04 22:31:23 +0100

    Refactor helper functions in helpers.js

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,53 +1,26 @@
-import { title, iconX, iconO, iconPerson, iconComputer } from './variables.js';
+import { title } from './variables.js';
 
-function moveLeft(item) {
+export function moveLeft(item) {
     item.classList.add('move-left');
 }
 
-function moveDown(item) {
+export function moveDown(item) {
     item.classList.add('move-down');
     hide(title);
 }
 
-function coverScreen(item) {
+export function coverScreen(item) {
     item.classList.add('expand');
 }
 
-function show(item) {
+export function show(item) {
     item.classList.remove('hidden');
 }
 
-function hide(item) {
+export function hide(item) {
     item.classList.add('hidden');
 }
 
-function addSelectedIconToElement(element, icon) {
-    if(icon === 'x') {
-        removeIconClass(element, iconO);
-        addSelectedIcon(element, iconX);
-    } else if(icon === 'o') {
-        removeIconClass(element, iconX);
-        addSelectedIcon(element, iconO);
-    } else if(icon === 'manual') {
-        removeIconClass(element, iconComputer);
-        addSelectedIcon(element, iconPerson);
-    } else if(icon === 'auto') {
-        removeIconClass(element, iconPerson);
-        addSelectedIcon(element, iconComputer);
-    }
-}
-
-function addSelectedIcon(element, iconClass) {
-    element.classList.add(iconClass);
-}
-
-function removeIconClass(element, iconClass) {
-    element.classList.remove(iconClass);
-}
-
-function getOpponentSymbol(selectedSymbol) {
+export function getOpponentSymbol(selectedSymbol) {
     return selectedSymbol === 'x' ? 'o' : 'x';
 }
-
-export {moveLeft, moveDown, coverScreen, show, hide, addSelectedIconToElement, getOpponentSymbol};
-
