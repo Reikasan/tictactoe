6c094a0c91cae36275772b478991ba1cfc2a1790
Reika Akuzawa <l06981@googlemail.com>
2024-03-19 10:39:08 +0100

    Add webpack configuration

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 
 import * as Cursor from './modules/cursor.js';
 import * as Game from './modules/game.js';
+
+import '../css/main.css';
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    }, 
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                        }
+                    ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 100 * 1024,
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            favicon: './favicon.ico',
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './site.webmanifest', to: './site.webmanifest'},
+            ],
+        }),
+    ],
+}
